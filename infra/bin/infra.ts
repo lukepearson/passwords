d93e722e6e8d4988c8b8b308648bb0178b2b4d2a
David Jimenez <dvejmz@sgfault.com>
2022-09-14 18:34:23 +0100

    Fix CDK stack parameter spelling

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -5,8 +5,12 @@ import { PasswordsStack } from '../lib/infra-stack';
 
 const { ACM_CERT_ID, S3_BUCKET_ARN } = process.env;
 
+if (!ACM_CERT_ID || !S3_BUCKET_ARN) {
+  throw new Error("Missing CDK stack parameters");
+}
+
 const app = new cdk.App();
 new PasswordsStack(app, 'PasswordsStack', {
-  AcmCertificateId: ACM_CERT_ID,
-  S3BucketArn: S3_BUCKET_ARN
+  acmCertificateId: ACM_CERT_ID,
+  s3BucketArn: S3_BUCKET_ARN
 });
