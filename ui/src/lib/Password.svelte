aa975e0ce419c62a35f9a7795e69f4722b08015c
David Jimenez <dvejmz@sgfault.com>
2022-09-27 22:13:13 +0100

    Animate popularity bars filling up with Svelte animations

--- ui/src/lib/Password.svelte
+++ ui/src/lib/Password.svelte
@@ -1,30 +1,35 @@
 <script>
   import { onMount } from 'svelte';
+  import { cubicInOut } from 'svelte/easing';
+
+  const fill = (node, { duration, fillPct }) => {
+    return {
+      duration,
+      css: t => {
+        return `
+          background: linear-gradient(to left, salmon 50%, rgba(0,0,0,1.0) 50%) -${(cubicInOut(t) * fillPct)}%;
+          background-size: 200%;
+        `;
+      }
+    }
+  }
 
   export let password;
   export let timesLeaked = 0;
   export let popularityPct = 0;
-  let loaded = '';
-  let popularityPctFormatted = `${100.0 - popularityPct.toFixed(2)}%`;
+  let popularityPctFormatted = `${(100.0 - popularityPct).toFixed(2)}%`;
   let timesLeakedFormatted = new Intl.NumberFormat('en-GB').format(timesLeaked);
-
-  onMount(() => {
-    loaded = 'loaded';
-  });
 </script>
-
-<div class="password-card {loaded}" style="--popularity-pct:{popularityPctFormatted}">
+<div class="password-card" in:fill="{{ duration: 2000, fillPct: popularityPct }}" style="--popularity-pct:{popularityPctFormatted}">
     <span class="password-term">{password}</span>
     <span class="password-subtitle">Found in {timesLeakedFormatted} leaks</span>
 </div>
 
 <style>
   .password-card {
-    background: black;
-    background: linear-gradient(to right, salmon 50%, rgba(0,0,0,1.0) 50%) var(--popularity-pct, 0%);
     color: white;
+    background: linear-gradient(to right, salmon 50%, rgba(0,0,0,1.0) 50%) var(--popularity-pct, 100%);
     background-size: 200%;
-    transition: 1s ease-out;
     border-radius: 35px;
     padding: 1em 1.5em;
   }
